e99b2908317d021a3367b92c726bf2f702530c61
Tshakalisa Khupe <88752230+tkhupe@users.noreply.github.com>
2023-02-18 19:51:21 -0600

    added methods

---
+++ script.js
@@ -0,0 +1,21 @@
+var constellations = ["Orion", "Scorpius", "Lyra", "Ursa Major", "Ursa Minor"];
+var planets = ["Earth", "Saturn", "Mars", "Jupiter", "Uranus", "Venus"];
+var star = "polaris";
+
+// WRITE YOUR CODE BELOW
+constellations.unshift('canis Major');
+
+console.log(constellations);
+
+planets.pop();
+
+console.log(planets);
+var galaxy = constellations.concat(planets);
+
+console.log(galaxy);
+console.log(planets);
+console.log(galaxy); 
+
+var capital = star.toUpperCase() 
+
+console.log(capital);
